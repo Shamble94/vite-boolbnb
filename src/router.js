f4daa84ba8ae271faac8c3aeadecaef378a4e852
SquallStrifeX <squallstrifex@gmail.com>
2024-04-04 01:05:28 +0200

    apartments+

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ApartmentDetails from "./components/ApartmentDetails.vue";
 import AppMain from "./components/AppMain.vue";
-import MessageForm from './components/MessageForm.vue';
+// import MessageForm from './components/MessageForm.vue';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -16,9 +16,9 @@ const router = createRouter({
             component: ApartmentDetails
         },
         {
-            path: '/message-form',
-            name: 'MessageForm', // Name of the route
-            component: MessageForm, // Component for the message form
+            // path: '/message-form',
+            // name: 'MessageForm', // Name of the route
+            // component: MessageForm, // Component for the message form
           },
         
     ]
