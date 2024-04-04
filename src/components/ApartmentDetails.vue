c729146458c1c3d36e182e2015912e3320b85aea
SquallStrifeX <squallstrifex@gmail.com>
2024-04-04 02:19:40 +0200

    ok

--- src/components/ApartmentDetails.vue
+++ src/components/ApartmentDetails.vue
@@ -4,165 +4,217 @@ import axios from "axios";
 import tt from "@tomtom-international/web-sdk-maps";
 
 export default {
-  name: "ApartmentDetail",
+    name: "ApartmentDetail",
 
-  data() {
-    return {
-      store,
-      apartment: null,
-      loader: false,
-      map: null,
-      apiKey: "GQoylkWTb8A3X4kupHH9BTdJj1GJaVKo",
-      marker: null,
-    };
-  },
-
-  created() {
-    this.getApartmentDetails();
-  },
-  methods: {
-    getApartmentDetails() {
-      axios
-        .get(`${this.store.baseUrl}/api/apartments/${this.$route.params.id}`)
-        .then((response) => {
-          this.apartment = response.data.result;
-          console.log(this.apartment);
-          this.loader = true;
-          this.initializeMap();
-        });
-    },
-    redirectToMessageForm() {
-      this.$router.push({ name: "MessageForm" });
+    data() {
+        return {
+            store,
+            apartment: null,
+            loader: false,
+            map: null,
+            apiKey: "GQoylkWTb8A3X4kupHH9BTdJj1GJaVKo",
+            marker: null,
+        };
     },
 
-    initializeMap() {
-      if (!this.map && this.apartment) {
-        const longitude = this.apartment.longitude;
-        const latitude = this.apartment.latitude;
-        console.log(longitude, latitude);
-
-        this.map = tt.map({
-          key: this.apiKey,
-          container: this.$refs.map,
-        });
-
-        this.map.setCenter([longitude, latitude]);
-        this.map.setZoom(16);
-      }
+    created() {
+        this.getApartmentDetails();
     },
-  },
+    methods: {
+        getApartmentDetails() {
+            axios
+                .get(`${this.store.baseUrl}/api/apartments/${this.$route.params.id}`)
+                .then((response) => {
+                    this.apartment = response.data.result;
+                    console.log(this.apartment);
+                    this.loader = true;
+                    this.initializeMap();
+                });
+        },
+        redirectToMessageForm() {
+            this.$router.push({ name: "MessageForm" });
+        },
+
+        initializeMap() {
+            if (!this.map && this.apartment) {
+                const longitude = this.apartment.longitude;
+                const latitude = this.apartment.latitude;
+                console.log(longitude, latitude);
+
+                this.map = tt.map({
+                    key: this.apiKey,
+                    container: this.$refs.map,
+                });
+
+                this.map.setCenter([longitude, latitude]);
+                this.map.setZoom(16);
+            }
+        },
+    },
 };
 </script>
 
 <template>
-  <div class="container" v-if="loader">
-    <div class="row">
-      <div class="col-4 m-5">
-        <!-- Bind the src attribute to the image URL if it exists, otherwise use a placeholder -->
-        <img
-          v-if="apartment.image !== '0'"
-          :src="`${store.baseUrl}/storage/${apartment.image}`"
-          class="card-img-top"
-          :alt="apartment.description"
-        />
-        <img
-          v-else
-          src="/placeholder2.png"
-          class="card-img-top"
-          :alt="apartment.description"
-        />
+    <div class="container" v-if="loader">
+        <div class="row">
+            <h1 class="apartments my-4">{{ apartment.description }}</h1>
+            <div class="col-md-6">
+                <!-- Immagine Principale -->
+                <div class="main-image">
+                    <img v-if="apartment.image !== '0'" :src="`${store.baseUrl}/storage/${apartment.image}`"
+                        class="card-img-top" :alt="apartment.description" />
+                    <img v-else src="/placeholder2.png" class="card-img-top" :alt="apartment.description" />
+                </div>
+            </div>
+            <div class="col-md-6">
+                <!-- Griglia per Immagini Secondarie -->
+                <div class="row">
+                    <div class="col-6 secondary-image ">
+                        <img v-if="apartment.image !== '0'" :src="`${store.baseUrl}/storage/${apartment.image}`"
+                            class="img-fluid" :alt="apartment.description">
 
-        <!-- Display apartment description -->
-        <p>{{ apartment.description }}</p>
+                    </div>
+                    <div class="col-6 secondary-image">
+                        <img v-if="apartment.image !== '0'" :src="`${store.baseUrl}/storage/${apartment.image}`"
+                            class="img-fluid" :alt="apartment.description">
 
-        <!-- Check if there are services -->
-        <div v-if="apartment.services && apartment.services.length > 0">
-          <h3>Services:</h3>
-          <!-- Loop through services and display their names -->
+                    </div>
+                    <div class="col-6 secondary-image mt-3">
+                        <img v-if="apartment.image !== '0'" :src="`${store.baseUrl}/storage/${apartment.image}`"
+                            class="img-fluid" :alt="apartment.description">
+                    </div>
+                    <div class="col-6 secondary-image mt-3">
+                        <img v-if="apartment.image !== '0'" :src="`${store.baseUrl}/storage/${apartment.image}`"
+                            class="img-fluid" :alt="apartment.description">
 
-          <ul class="list-unstyled">
-            <li v-for="(service, index) in apartment.services" :key="index">
-              <!-- Ensure proper class binding for the icon -->
-              <i :class="service.icon"></i
-              ><span class="mx-2">{{ service.name }}</span>
-            </li>
-          </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="apartment-details">
+                <p class="fs-3 fw-semibold my-3"> {{ apartment.location }}, Indirizzo per esteso da aggiungere</p>
+                <div class="fs-5">
+                    <span>{{ apartment.rooms }} Camere • </span>
+                    <span>{{ apartment.beds }} Letti •</span>
+                    <span>{{ apartment.bathrooms }} Bagni •</span>
+                    <span>{{ apartment.square_meters }} m²</span>
+                </div>
+            </div>
+            <!-- Check if there are services -->
+
         </div>
-        <div>
-          <!-- Apartment details content -->
-
-          <!-- Button to navigate to the message form -->
-          <button class="p-1" @click="redirectToMessageForm">
-            Chiedi informazioni
-          </button>
-
-          <div></div>
-        </div>
-      </div>
     </div>
-  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <p>Servizi:</p>
+                <!-- <div v-if="apartment.services && apartment.services.length > 0">
+                    <h3>Services:</h3>
 
-  <div ref="map" style="width: 100%; height: 100%"></div>
+
+                    <ul class="list-unstyled">
+                        <li v-for="(service, index) in apartment.services" :key="index">
+
+                            <i :class="service.icon"></i><span class="mx-2">{{ service.name }}</span>
+                        </li>
+                    </ul>
+                </div> -->
+            </div>
+            <div class="col-6 mapx">
+                <p class="fs-3 fw-bold ">Dove sarai</p>
+                <div ref="map" id="map"></div>
+                <!-- Assicurati di dare un'altezza esplicita -->
+
+            </div>
+        </div>
+    </div>
+    <button class="p-1" @click="redirectToMessageForm">
+        Chiedi informazioni
+    </button>
+
+
 </template>
 
 <style lang="scss" scoped>
 img {
-  width: 400px;
+    width: 400px;
 }
 
 .apartment-details p {
-  font-size: 20px;
+    font-size: 20px;
 }
+
 .services-list {
-  display: flex;
-  flex-wrap: wrap;
-  list-style: none;
-  padding: 0;
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+    padding: 0;
 }
+
 .service-item {
-  margin: 10px;
-  display: flex;
-  align-items: center;
-  i {
-    margin-right: 5px;
-  }
+    margin: 10px;
+    display: flex;
+    align-items: center;
+
+    i {
+        margin-right: 5px;
+    }
 }
+
 .apartments {
-  font-size: 35px;
+    font-size: 35px;
 }
+
 .img {
-  display: flex;
-  justify-content: center;
+    display: flex;
+    justify-content: center;
 }
+
 .back-main {
-  background-color: yellow;
+    background-color: yellow;
 }
+
 /* Stili SCSS */
 .image-container {
-  width: 100%; /* o una larghezza fissa a seconda delle tue esigenze */
-  padding-top: 100%; /* Questo mantiene l'aspetto quadrato */
-  background-size: cover;
-  background-position: center;
-  position: relative;
+    width: 100%;
+    /* o una larghezza fissa a seconda delle tue esigenze */
+    padding-top: 100%;
+    /* Questo mantiene l'aspetto quadrato */
+    background-size: cover;
+    background-position: center;
+    position: relative;
 }
 
 /* Assicurati che il contenitore dell'immagine mantenga il suo aspetto quadrato anche in responsive */
 @media (max-width: 768px) {
-  .image-container {
-    width: 100%; /* Ajusta secondo necessità */
-    padding-top: 100%;
-  }
+    .image-container {
+        width: 100%;
+        /* Ajusta secondo necessità */
+        padding-top: 100%;
+    }
 }
+
 .secondary-image img {
-  width: 100%;
-  height: auto;
-  display: block; /* Assicura che le immagini si estendano per tutta la larghezza del contenitore */
-  object-fit: cover; /* Opzionale: assicura che le immagini coprano lo spazio disponibile senza perdere le proporzioni */
+    width: 100%;
+    height: auto;
+    display: block;
+    /* Assicura che le immagini si estendano per tutta la larghezza del contenitore */
+    object-fit: cover;
+    /* Opzionale: assicura che le immagini coprano lo spazio disponibile senza perdere le proporzioni */
 }
 
 .main-image img {
-  width: 100%;
-  height: auto;
-  display: block;
+    width: 100%;
+    height: auto;
+    display: block;
+}
+
+#map::hover{
+ width: 100%;
+ height: 100%;
+
+}
+
+.mapx{
+    border: solid rgb(177, 176, 176) 1px;
 }
 </style>
