4692bbb21e177b12fda5dade6dbfe3d74650cd53
nicolocamassa <camassanicolo03@gmail.com>
2024-04-08 18:21:50 +0200

    [FIX] home fixed

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -8,10 +8,10 @@ export default {
 </script>
 
 <template>
-  <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 my-2">
-    <router-link
+  <div class="col-xl-2 col-lg-3">
+    <router-link 
       :to="{ name: 'ApartmentDetails', params: { id: card.id } }"
-      class="text-deco m-3">
+      class="text-decoration-none me-3">
       <div class="my-card">
         <div class="img-container">
           <img
@@ -28,14 +28,19 @@ export default {
           />
         </div>
 
-        <div class="d-flex align-items-center p-2">
-          <h5 class="card-title text-truncate colore-viola larghezza">{{ card.location }}</h5><br>
-          <h6 class="card-title text-truncate colore-viola larghezza">{{ card.address }}</h6>
+        <h2>{{ card.address }}</h2>
+        <p>{{ card.bathrooms }} Bagni • {{ card.beds }} Letti • {{ card.rooms }} Stanze</p>
+
+
+
+        <!-- <div class="d-flex align-items-center p-2">
+          <h5 class="card-title text-truncate colore-viola larghezza">{{ card.address }}</h5>
           <div>
             <span class="bordo text-light p-2 m-1 sfondo-viola d-inline-block"><i class="fa-solid fa-bath mx-1"></i>{{ card.rooms }} </span> <br>
             <span class="bordo text-light p-2 m-1 sfondo-viola d-inline-block"><i class="fa-solid fa-bed mx-1"></i>{{ card.beds }}</span>
           </div>
         </div>
+        </div> -->
       </div>
     </router-link>
   </div>
@@ -43,7 +48,29 @@ export default {
 
 <style lang="scss">
 @use "../style/general.scss";
-.my-card {
+
+.img-container {
+  width: 270px;
+  height: 270px;
+  overflow: hidden; 
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover; 
+    border-radius: 15px;
+  }
+}
+h2{
+  font-weight: 700;
+  color: black;
+  margin-top: 10px;
+  font-size: 20px;
+}
+p{
+  color: rgb(116, 116, 116);
+  margin-top: -5px;
+}
+/* .my-card {
   width: 100%; 
   background-color: rgb(255, 255, 255);
   border-radius: 20px;
@@ -56,19 +83,17 @@ a {
     text-decoration: none; 
     color: inherit; 
 }
-
 .bordo{
   background-color: #5968EF;
   border-radius: 15px;
 }
-
 .larghezza{
   width: 70%;
 }
-
 .img-top{
   width: 100%;
   height: 260px;
   object-fit: cover;
 }
+} */
 </style>
